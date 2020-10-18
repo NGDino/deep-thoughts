6384d90bb659ae51ebe521a11ce4c894c5beba86
Neil Dino <N.dino90@gmail.com>
2020-10-17 21:01:11 -0600

    set up typeDefs and resolvers

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+// import AplolloServer
+const { ApolloServer } = require('apollo-server-express')
+//import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+//create a new Apollo server and pass schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+//integrate apollo server with express application as middleware
+server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -10,5 +22,7 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    //log where we can go to test gql api
+    console.log(`use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
